31c19109df4cc40de11a1fc7fe865c4164bf99c8
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2023-02-25 14:44:50 +0200

    add default component

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedRoutingModule } from './shared-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { DefaultComponent } from './components/default/default.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DefaultComponent
+  ],
   imports: [
     CommonModule,
     BrowserModule,
